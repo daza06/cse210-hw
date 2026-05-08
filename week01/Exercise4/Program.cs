c94745808bbd0017c45403ac577b05e74b60989d
Daniel Zavala <link123852@gmail.com>
2026-05-08 17:15:07 -0600

    Completed Week 1 Exercise 4 and 5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+        List<int> numbers = new List<int>();
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double avg = (double)sum / numbers.Count;
+        avg = Math.Round(avg, 2);
+        Console.WriteLine($"The average is: {avg}");
+
+        int largest = numbers.Max();
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
+
 }
