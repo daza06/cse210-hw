487e671e7ac73eb56529d7f0a52ad8440f489109
Daniel Zavala <link123852@gmail.com>
2026-06-06 16:42:12 -0600

    Completed Week 4 Online Ordering Program

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,48 @@
+using System.Text;
+public class Order
+{
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product p in _products)
+        {
+            total += p.GetTotalCost();
+        }
+
+        total += _customer.LivesInUSA() ? 5 : 35;
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine("Packing Label:");
+        foreach (Product p in _products)
+        {
+            sb.AppendLine(p.GetPackingInfo());
+        }
+
+        return sb.ToString();
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetShippingInfo()}";
+    }
+}
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("456 Main St", "Fort Collins", "Colorado", "USA");
+        Customer customer1 = new Customer("John Doe", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("PC", "P01", 1000, 1));
+        order1.AddProduct(new Product("Keyboard", "P02", 35, 5));
+
+        Address address2 = new Address("234 Coyote Ave", "Versailles", "Paris", "France");
+        Customer customer2 = new Customer("Maria Silva", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Mouse", "P03", 50, 1));
+        order2.AddProduct(new Product("Monitor", "P04", 300, 1));
+
+        Console.WriteLine("ORDER 1:");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order1.GetTotalCost()}");
+
+        Console.WriteLine("\nORDER 2:");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order2.GetTotalCost()}");
     }
 }
