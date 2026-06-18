81d1ec7394fe66b49017819e77d00006539b7e51
Daniel Zavala <link123852@gmail.com>
2026-06-18 16:38:16 -0600

    Completed Week 7 Project

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running("03 Nov 2022", 30, 3.0);
+        Cycling cycling = new Cycling("04 Nov 2022", 45, 12.0);
+        Swimming swimming = new Swimming("05 Nov 2022", 20, 40);
+
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
